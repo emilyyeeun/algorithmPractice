02fdc393ca6743a2db1c0f838f66dd8ceafdd72e
Yeeun Hong <yeeunh@alumni.upenn.edu>
2024-04-19 15:05:04 +0900

    Create 42889.java

---
+++ practice3/42889.java
@@ -0,0 +1,36 @@
+// 1차 시도 (fail) 
+import java.util.*;
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        HashMap<Integer, Double> percent = new HashMap<>();
+
+        for (int i = 1; i < N + 1; i++) {
+            int curr = i;
+            int total = (int) Arrays.stream(stages).filter(k -> k >= curr).count();
+            int fail = (int) Arrays.stream(stages).filter(k -> k == curr).count();
+            percent.put(curr, ((double) fail / total));
+        }
+        
+        List<Integer> keySet = new ArrayList<>(percent.keySet());
+        keySet.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (percent.get(o1) > percent.get(o2)) {
+                    return -1;
+                } else if (percent.get(o1) == percent.get(o2)) {
+                    return o1 - o2;
+                } else {
+                    return 1;
+                }
+            }
+        });
+        
+        int[] result = new int[N];
+        int i = 0;
+        for (Integer key : keySet) {
+            result[i] = key;
+            i++;
+        }
+        return result;
+    }
+}
